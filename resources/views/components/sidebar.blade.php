3c3a3cd91a3957fcea6d510480e738e9bcb6c4dd
Taupik Pirdian <taupik.pirdian@salt.co.id>
2025-11-10 22:27:44 +0700

    feat: html dashboard

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -51,7 +51,7 @@
                 <li>
                     <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                         <i class="material-symbols-outlined">lab_profile</i>
-                        <span class="nav-text">User Management</span>
+                        <span class="nav-text">User</span>
                     </a>
                     <ul aria-expanded="false">
                         <li><a href="{{ route('users.index') }}">User</a></li>
@@ -62,7 +62,7 @@
                 <li>
                     <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                         <i class="material-symbols-outlined">lab_profile</i>
-                        <span class="nav-text">Bagian Management</span>
+                        <span class="nav-text">Internal Data</span>
                     </a>
                     <ul aria-expanded="false">
                         <li><a href="{{ route('divisions.index') }}">Sub Bagian</a></li>
